61fd9b621dee86ca7d48c11dbae1a164a7264980
Ali Gelenler <ali.gelenler@gmail.com>
2017-02-25 14:07:13 +0100

    Add files via upload

---
+++ java/Memory/DirectMemory/GenericEntity.java
@@ -0,0 +1,25 @@
+package tr.com.aligelenler.entity;
+
+import java.io.Serializable;
+
+public class GenericEntity  implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1543582778125579346L;
+
+	
+	private boolean canEdit;
+
+	@javax.persistence.Transient
+	public boolean isCanEdit() {
+		return canEdit;
+	}
+
+	public void setCanEdit(boolean canEdit) {
+		this.canEdit = canEdit;
+	}
+
+
+}
